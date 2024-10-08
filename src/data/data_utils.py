2b4bafac35b663ab39d7ea69add05d72e5e755c9
danielliu-meridian <danielliu@meridianinno.com>
2024-10-08 18:26:06 +0800

    update readme

--- src/data/data_utils.py
+++ src/data/data_utils.py
@@ -1,25 +1,33 @@
-import tensorflow as tf
-import os
 
-def load_dataset(data_dir, batch_size):
-    # Assume data_dir contains 'clean' and 'noisy' subdirectories with images
-    clean_images = tf.data.Dataset.list_files(os.path.join(data_dir, 'clean', '*.png'))
-    noisy_images = tf.data.Dataset.list_files(os.path.join(data_dir, 'noisy', '*.png'))
+def get_dataloaders(data_root_copy: str, batchsize: int):
+    """function to create tensorflow data loaders for model training/validation/testing
+    Args:
+        data_root_copy (str): directory path of preprocessed raw input folder copy
+        batchsize (int): batch size
+    Returns:
+        _type_: tensors for corresponding batched inputs/outputs
+    """
+    inputs = []
+    outputs = []
+    for filename in os.listdir(data_root_copy):
+        input_, output_ = parse_input_output_pairs(data_root_copy, gold_root_copy, filename)
+        inputs.append(input_)
+        outputs.append(output_)
 
-    # Load and preprocess images
-    def process_image(file_path):
-        image = tf.io.read_file(file_path)
-        image = tf.image.decode_png(image, channels=1)
-        image = tf.image.convert_image_dtype(image, tf.float32)
-        return image
+    inputs, outputs = tf.cast(tf.stack(inputs, axis=0), tf.float32), tf.cast(tf.stack(outputs, axis=0), tf.float32)
+    print(type(inputs), inputs.shape)
+    print(type(outputs), outputs.shape)
 
-    clean_dataset = clean_images.map(process_image, num_parallel_calls=tf.data.AUTOTUNE)
-    noisy_dataset = noisy_images.map(process_image, num_parallel_calls=tf.data.AUTOTUNE)
+    gfg_inputs = tf.data.Dataset.from_tensor_slices(inputs)
+    gfg_outputs = tf.data.Dataset.from_tensor_slices(outputs)
+    gfg_inputs_loader = gfg_inputs.batch(batchsize)
+    gfg_outputs_loader = gfg_outputs.batch(batchsize)
 
-    # Zip datasets together and prepare batches
-    dataset = tf.data.Dataset.zip((noisy_dataset, clean_dataset))
-    dataset = dataset.shuffle(buffer_size=1000)
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.prefetch(tf.data.AUTOTUNE)
+    return gfg_inputs_loader, gfg_outputs_loader
 
-    return dataset
+
+if __name__ == "__main__":
+    data_root = r"C:/Users/takao/Desktop/denoising_collected_data/raw_imgs"
+    gold_root = r"C:/Users/takao/Desktop/denoising_collected_data/gold_standards"
+    preprocess(data_root, gold_root)
+    print("FINISHED PREPROCESSING.")
