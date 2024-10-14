9e2d160df90a0d9e98d0d59043434d18d7ff80bc
Danielliu-meridian <danielliu@meridianinno.com>
2024-10-14 10:51:29 +0800

    configuration

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,13 +1,14 @@
 # src/utils/utils.py
 import yaml
 import tensorflow as tf
-import os 
+from tf.keras.saving import register_keras_serializable
 
-def load_config(config_path):
-    with open(config_path, 'r') as f:
-        config = yaml.safe_load(f)
-    return config
+# def load_config(config_path):
+#     with open(config_path, 'r') as f:
+#         config = yaml.safe_load(f)
+#     return config
 
 # define loss function
+@register_keras_serializable(package='Custom', name='loss_function')
 def loss_function(y_true, y_pred):
     return tf.reduce_mean(tf.abs(y_true - y_pred))
