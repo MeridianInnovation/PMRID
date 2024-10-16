003c96fb17b115466cb742a6daf1128cb21f1d02
danielliu-meridian <danielliu@meridianinno.com>
2024-10-16 18:20:16 +0800

    update hypers

---
+++ src/utils/hyperparameters.py
@@ -0,0 +1,41 @@
+
+import yaml
+import os
+
+# Define a class to store hyperparameters
+# This class will be used to store hyperparameters for the training process
+class Hyperparameters:
+    # def __init__(self, learning_rate=1e-5, batch_size=64, epochs=20, optimizer='SGD', momentum=0.0):
+    #     self.learning_rate = learning_rate
+    #     self.batch_size = batch_size
+    #     self.epochs = epochs
+    #     self.optimizer = optimizer
+    #     self.momentum = momentum
+
+    def __init__(self, config_file):
+        self.load_hyperparameters(config_file)
+
+    def load_hyperparameters(self, config_file):
+        """ Load hyperparameters from a config file """
+        with open(config_file, 'r') as file:
+            config = yaml.safe_load(file)
+        
+        # Set the hyperparameters
+        self.learning_rate = config['learning_rate']
+        self.batch_size = config['batch_size']
+        self.epochs = config['epochs']
+        self.optimizer = config['optimizer']
+        self.momentum = config['momentum']
+
+    def print_params(self):
+        print(f"Learning Rate: {self.learning_rate}")
+        print(f"Batch Size: {self.batch_size}")
+        print(f"Epochs: {self.epochs}")
+        print(f"Optimizer: {self.optimizer}")
+        print(f"Momentum: {self.momentum}")
+
+if __name__ == "__main__":
+    # print(os.getcwd()
+    path_to_config = os.path.join(os.getcwd(), 'configs/hyperparameters.yaml')
+    hyperparams = Hyperparameters(path_to_config)
+    hyperparams.print_params()
