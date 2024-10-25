7e53d8d2d001c5b45d9e2f2cb5e6030195b81cc2
danielliu-meridian <danielliu@meridianinno.com>
2024-10-25 16:39:29 +0800

    data utils for pytorch

--- src/data/data_utils_torch.py
+++ src/data/data_utils_torch.py
@@ -1,6 +1,8 @@
 # data_loader_utils.py
 from torch.utils.data import DataLoader
 import os
+from PIL import Image
+import torchvision.transforms as transforms
 
 def decode_image(file_path):
     """
@@ -10,8 +12,21 @@ def decode_image(file_path):
         file_path: The path to the image file.
 
       Returns:
-        image: The preprocessed image in float32 format.
+        image: The preprocessed image.
     """
+    image = Image.open(file_path)
+
+    # Define the transformations to be applied to the image
+    transform = transforms.Compose([
+        transforms.Resize((120, 160)), # Resize the image to 120x160
+        transforms.ToTensor(), # Convert the image to a PyTorch Tensor (C x H x W)
+        # transforms.Normalize(mean=[0.5], std=[0.5]) # Normalize the image
+    ])
+
+    # Apply the transformations to the image
+    image = transform(image)
+
+    return image
 
 def create_dataset(noisy_folder_dir, clean_folder_dir):
     """
@@ -25,6 +40,15 @@ def create_dataset(noisy_folder_dir, clean_folder_dir):
         noisy_dataset: The dataset containing the noisy images.
         clean_dataset: The dataset containing the clean images.
     """
+    # Get the list of files in folders
+    noisy_files = os.listdir(noisy_folder_dir)
+    clean_files = os.listdir(clean_folder_dir)
+
+    # Decode images
+    noisy_dataset = [decode_image(os.path.join(noisy_folder_dir, file)) for file in noisy_files]
+    clean_dataset = [decode_image(os.path.join(clean_folder_dir, file)) for file in clean_files]
+
+    return noisy_dataset, clean_dataset
 
 
 def prepare_dataloaders(root_dir, batch_size):
@@ -41,10 +65,10 @@ def prepare_dataloaders(root_dir, batch_size):
         validation_loader: The validation data loader
     """
     # Define the paths to the clean and noisy folders for training and validation
-    train_noisy_folder_dir = os.path.join(root_dir, 'images_thermal_train_resized_noisy')
-    train_clean_folder_dir = os.path.join(root_dir, 'images_thermal_train_resized_clean')
-    val_noisy_folder_dir = os.path.join(root_dir, 'images_thermal_val_resized_noisy')
-    val_clean_folder_dir = os.path.join(root_dir, 'images_thermal_val_resized_clean')
+    train_noisy_folder_dir = os.path.join(root_dir, 'reduced_dataset_1_8', 'images_thermal_train_resized_noisy')
+    train_clean_folder_dir = os.path.join(root_dir, 'reduced_dataset_1_8', 'images_thermal_train_resized_clean')
+    val_noisy_folder_dir = os.path.join(root_dir, 'reduced_dataset_1_8', 'images_thermal_val_resized_noisy')
+    val_clean_folder_dir = os.path.join(root_dir, 'reduced_dataset_1_8', 'images_thermal_val_resized_clean')
 
     # Create Dataset for training and validation
     train_noisy_dataset, train_clean_dataset = create_dataset(train_noisy_folder_dir, train_clean_folder_dir)
@@ -56,3 +80,26 @@ def prepare_dataloaders(root_dir, batch_size):
     validation_loader = DataLoader(list(zip(val_noisy_dataset, val_clean_dataset)), batch_size=batch_size, shuffle=False)
 
     return training_loader, validation_loader
+
+if __name__ == '__main__':
+    # print current working directory
+    print(os.getcwd())
+
+    root_dir = 'data'
+    batch_size = 128
+
+    training_loader, validation_loader = prepare_dataloaders(root_dir, batch_size)
+
+    print(f'Training data loader: {training_loader}')
+    print(f'Validation data loader: {validation_loader}')
+
+    # Print the first batch of data
+    for noisy_images, clean_images in training_loader:
+        print(f'Noisy images: {noisy_images.shape}')
+        print(f'Clean images: {clean_images.shape}')
+        break
+    # Print the first batch of data
+    for noisy_images, clean_images in validation_loader:
+        print(f'Noisy images: {noisy_images.shape}')
+        print(f'Clean images: {clean_images.shape}')
+        break
