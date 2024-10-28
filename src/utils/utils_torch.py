b342abf71976b5f7ed37f8f003ceb7d95743ff4c
danielliu-meridian <danielliu@meridianinno.com>
2024-10-28 09:10:49 +0800

    add l1 loss torch

---
+++ src/utils/utils_torch.py
@@ -0,0 +1,24 @@
+import torch
+
+# loss_fn_f1 = torch.nn.L1Loss()
+
+import torch
+import torch.nn.functional as F
+
+def loss_fn_f1(pred: torch.Tensor, label: torch.Tensor):
+    B = pred.shape[0]
+    L1 = F.l1_loss(pred, label, reduction='none').view(B, -1).mean(dim=1)
+    return L1.mean()
+
+if __name__ == '__main__':
+    # Set the seed 
+    torch.manual_seed(0)
+    # Define the input and target tensors
+    input_tensor = torch.randn(3, 5, requires_grad=True)
+    target_tensor = torch.randn(3, 5)
+    print(input_tensor)
+    print(target_tensor)
+
+    # Compute the loss
+    loss = loss_fn_f1(input_tensor, target_tensor)
+    print('Loss:', loss.item())
